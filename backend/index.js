e10ea9aa9703eda2f3be940ea448cd9d995a1895
simplest-ayush <ujjwal301004@gmail.com>
2025-08-11 01:15:40 +0530

    added frontend url in cors

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,11 @@ dotenv.config({
     path: '../.env'
 })
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://shrinkrr.netlify.app',
+}))
 app.use(express.json())
+app.use(express.static("public"))
 
 const PORT = process.env.PORT || 8000
 
