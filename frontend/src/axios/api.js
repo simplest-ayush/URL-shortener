9eac55959b023ae0bfdb34c4733108a6194f93b8
simplest-ayush <ujjwal301004@gmail.com>
2025-08-11 10:11:29 +0530

    changed the backend url

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://url-shortener-n3lc.onrender.com'
 })
 
 export default instance
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://url-shortener-n3lc.onrender.com',
         changeOrigin: true
       }
     }
