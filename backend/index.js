084ea21dc4ca119ab80e958b7fe826fa6fd09419
simplest-ayush <ujjwal301004@gmail.com>
2025-08-11 10:05:38 +0530

    removed minor bugs from index.js

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './db/index.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './db/index.js'
 import urlRoutes from './routes/url.route.js';
-import path from 'path';
 
-dotenv.config({ path: '../.env' });
+const app = express()
 
-const app = express();
-const __dirname = path.resolve();
-const PORT = process.env.PORT || 8000;
+dotenv.config({
+    path: '../.env'
+})
 
-// CORS
 app.use(cors({
-    origin: 'https://shrinkrr.netlify.app',
-}));
+    origin: 'https://shrinkrr.netlify.app'
+}))
+app.use(express.json())
 
-app.use(express.json());
+const PORT = process.env.PORT || 8000
 
-// Serve static files from the Vite build folder
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// API routes
-app.use('/api', urlRoutes);
-
-// Catch-all route for React Router (must come last)
-app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
-
-// Connect to DB and start server
 connectDB()
     .then(() => {
+        app.on('error', (error) => {
+            console.log("ERRR ", error);
+        })
         app.listen(PORT, () => {
             console.log(`⚙️ Server is running on port ${PORT}`);
-        });
+
+        })
     })
     .catch((error) => {
-        console.log('MongoDB connection failed : ', error);
-    });
+        console.log("MongoDB connection failed : ", error)
+    })
+
+app.use('/', urlRoutes);
